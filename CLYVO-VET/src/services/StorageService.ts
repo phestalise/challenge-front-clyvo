6ade7f96e17392d268bb99a63707581bdc3aaeb4
phestalise <phestalise@gmail.com>
2026-05-05 20:44:46 -0300

    adicionando os services e component

---
+++ CLYVO-VET/src/services/StorageService.ts
@@ -0,0 +1,44 @@
+import AsyncStorage from "@react-native-async-storage/async-storage";
+import { User, Pet } from "../types";
+import { IStorage } from "../../../interfaces/IStorage";
+
+const KEYS = {
+  USER: "@clyvo:user",
+  PETS: "@clyvo:pets",
+  LOGGED_IN: "@clyvo:loggedIn",
+};
+
+class StorageService implements IStorage {
+  async saveUser(user: User): Promise<void> {
+    await AsyncStorage.setItem(KEYS.USER, JSON.stringify(user));
+  }
+
+  async getUser(): Promise<User | null> {
+    const data = await AsyncStorage.getItem(KEYS.USER);
+    return data ? JSON.parse(data) : null;
+  }
+
+  async savePets(pets: Pet[]): Promise<void> {
+    await AsyncStorage.setItem(KEYS.PETS, JSON.stringify(pets));
+  }
+
+  async getPets(): Promise<Pet[]> {
+    const data = await AsyncStorage.getItem(KEYS.PETS);
+    return data ? JSON.parse(data) : [];
+  }
+
+  async setLoggedIn(value: boolean): Promise<void> {
+    await AsyncStorage.setItem(KEYS.LOGGED_IN, JSON.stringify(value));
+  }
+
+  async getLoggedIn(): Promise<boolean> {
+    const data = await AsyncStorage.getItem(KEYS.LOGGED_IN);
+    return data ? JSON.parse(data) : false;
+  }
+
+  async clearAll(): Promise<void> {
+    await AsyncStorage.multiRemove([KEYS.USER, KEYS.PETS, KEYS.LOGGED_IN]);
+  }
+}
+
+export const storageService = new StorageService();
